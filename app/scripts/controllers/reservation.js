11096c480a095ce1d4d59fb3f6d948431301c3f6
aisensiy <aisensiy@gmail.com>
2015-01-31 02:00:30 +0800

    reservation scaffold

---
+++ app/scripts/controllers/reservation.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pickypickApp.controller:ReservationCtrl
+ * @description
+ * # ReservationCtrl
+ * Controller of the pickypickApp
+ */
+angular.module('pickypickApp')
+  .controller('ReservationCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
