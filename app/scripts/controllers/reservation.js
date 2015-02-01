7a8b7c5ecf252f8960c89b9b75fae01be0518ff5
aisensiy <aisensiy@gmail.com>
2015-02-02 00:37:34 +0800

    view for reservation

--- app/scripts/controllers/reservation.js
+++ app/scripts/controllers/reservation.js
@@ -9,9 +9,38 @@
  */
 angular.module('pickypickApp')
   .controller('ReservationCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    var dishes = [
+      {
+        'name': '香汁排骨饭汤套餐',
+        'price': 29.5,
+        'restaurant': '真功夫'
+      },
+      {
+        'name': '冬菇鸡腿饭汤套餐',
+        'price': 27,
+        'restaurant': '真功夫'
+      },
+      {
+        'name': '鱼香茄子饭汤套餐',
+        'price': 20,
+        'restaurant': '真功夫'
+      },
+      {
+        'name': '排骨拼鸡腿肉饭汤套餐',
+        'price': 30.5,
+        'restaurant': '真功夫'
+      }
     ];
+    $scope.dishes = dishes;
+    $scope.dishes.forEach(function(dish) {
+      dish.count = 0;
+    });
+
+    $scope.count_plus = function(dish) {
+      dish.count += 1;
+    };
+    $scope.count_minus = function(dish) {
+      if (dish.count <= 0) return;
+      dish.count -= 1;
+    };
   });
