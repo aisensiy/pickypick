643b9b5c81eb8e2154de0e0360e51e4b968126a2
aisensiy <aisensiy@gmail.com>
2015-02-05 22:21:22 +0800

    add scaffold for dish-stat

---
+++ app/scripts/controllers/dish-stat.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pickypickApp.controller:DishStatCtrl
+ * @description
+ * # DishStatCtrl
+ * Controller of the pickypickApp
+ */
+angular.module('pickypickApp')
+  .controller('DishStatCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
