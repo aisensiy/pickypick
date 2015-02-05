63fa826e19240da2d19cd39ed872a065771b8a8d
aisensiy <aisensiy@gmail.com>
2015-02-05 23:23:49 +0800

    dish detail scaffold

---
+++ app/scripts/controllers/dish_detail.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pickypickApp.controller:DishDetailCtrl
+ * @description
+ * # DishDetailCtrl
+ * Controller of the pickypickApp
+ */
+angular.module('pickypickApp')
+  .controller('DishDetailCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
