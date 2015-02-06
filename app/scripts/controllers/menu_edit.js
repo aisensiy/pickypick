011a7778e72c18ba98feb4d76c12597972ee1e7c
aisensiy <aisensiy@gmail.com>
2015-02-06 23:04:14 +0800

    mv menu edit page data service to api dish and menu

--- app/scripts/controllers/menu_edit.js
+++ app/scripts/controllers/menu_edit.js
@@ -8,55 +8,34 @@
  * Controller of the pickypickApp
  */
 angular.module('pickypickApp')
-  .controller('MenuEditCtrl', function ($scope) {
-    var dishes = [
-      {
-        'name': '香汁排骨饭汤套餐',
-        'price': 29.5,
-        'restaurant': '真功夫'
-      },
-      {
-        'name': '冬菇鸡腿饭汤套餐',
-        'price': 27,
-        'restaurant': '真功夫'
-      },
-      {
-        'name': '鱼香茄子饭汤套餐',
-        'price': 20,
-        'restaurant': '真功夫'
-      },
-      {
-        'name': '排骨拼鸡腿肉饭汤套餐',
-        'price': 30.5,
-        'restaurant': '真功夫'
-      }
-    ];
-    $scope.dishes = dishes;
-    $scope.menu = [];
+  .controller('MenuEditCtrl', ['$scope', 'DishAPI', 'MenuAPI', function ($scope, DishAPI, MenuAPI) {
+    DishAPI.query(function(dishes) {
+      $scope.dishes = dishes;
+    });
+    MenuAPI.create(function(menu) {
+      $scope.menu = menu;
+    })
     $scope.new_dish = {};
-
     $scope.selected = {};
 
     $scope.add_new_dish = function() {
-      $scope.menu.push($scope.new_dish);
-      $scope.new_dish = {};
+      MenuAPI.add_dish_to_menu($scope.menu, $scope.new_dish, function() {
+        $scope.new_dish = {};
+      });
     };
 
     $scope.remove_dish = function(dish) {
-      for (var i = 0; i < $scope.menu.length; i++) {
-        if (dish == $scope.menu[i]) {
-          $scope.menu.splice(i, 1);
-          delete $scope.selected[dish.name];
-          break;
-        }
-      }
+      MenuAPI.remove_dish_from_menu($scope.menu, dish, function() {
+        delete $scope.selected[dish.name];
+      });
     };
 
     $scope.copy_to_menu = function(dish) {
       if ($scope.selected[dish.name]) {
         return;
       }
-      $scope.menu.push(Object.create(dish));
-      $scope.selected[dish.name] = true;
+      MenuAPI.add_dish_to_menu($scope.menu, Object.create(dish), function() {
+        $scope.selected[dish.name] = true;
+      });
     };
-  });
+  }]);
