1741683a031555eae101e9cad539dfb0f59e4253
aisensiy <aisensiy@gmail.com>
2015-02-09 16:22:18 +0800

    add tips for empty set

--- app/scripts/controllers/dish_detail.js
+++ app/scripts/controllers/dish_detail.js
@@ -10,9 +10,15 @@
  */
 angular.module('pickypickApp')
   .controller('DishDetailCtrl', ['$scope', 'OrderAPI', '$routeParams', function ($scope, OrderAPI, $routeParams) {
+    $scope.total_price = 0;
     OrderAPI.order_list($routeParams.menu_id, function(orders) {
       $scope.$apply(function() {
         $scope.orders = orders;
+        angular.forEach($scope.orders, function(order) {
+          angular.forEach(order.dishes, function(dish) {
+            $scope.total_price += dish.price;
+          });
+        });
       });
     });
     $scope.update_order_pay_info = function(order) {
